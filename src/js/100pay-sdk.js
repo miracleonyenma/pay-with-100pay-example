d3f2628b1d377e21abe5c4895fe4916e1ba0af37
miracleonyenma <miracleficient@gmail.com>
2022-03-15 18:57:18 +0100

    updates && fixes

--- src/js/100pay-sdk.js
+++ src/js/100pay-sdk.js
@@ -1,1 +1,140 @@
-let charge; class payWith100Pay { constructor(e) { } appendStyle(e) { let t = document.createElement("STYLE"); t.id = "100pay_style", t.type = "text/css", t.appendChild(document.createTextNode(e)), document.head.appendChild(t) } setup(e) { charge = e; let t = charge.api_key; delete charge.callback, delete charge.api_key; let n = charge; fetch("https://api.shop100.co/api/v1/pay/charge", { method: "POST", headers: { "Content-Type": "application/json", "api-key": t }, body: JSON.stringify(n) }).then((e => e.json())).then((e => { this.createElements(e) })).catch((e => { charge.onError(e) })) } createElements(e) { this.appendStyle("\n        #show100Pay {\n        top: 0;\n        left: 0;\n        position: fixed;\n        background-color: #000000c2;\n        width: 100vw;\n        height: 100vh;\n        z-index: 1050;\n        display: none;\n        }\n        .show100Pay {\n        transition: 0.3s;\n        height: 100vh;\n        width: 100%;\n        max-width: 400px;\n        left: 50%;\n        transform: translateX(-50%);\n        position: fixed;\n        z-index: 1050;\n        top: 100%;\n        padding-top: 0;\n        border-radius: 16px 16px 0 0;\n        overflow: hidden;\n      }\n      .show100Pay .show100Pay_modal {\n        height: calc(100% - 8%);\n        width: 100%;\n        border: none;\n        border-radius: 16px 16px 0 0;\n        padding-bottom: 0;\n        }\n        .show100Pay.show {\n        top: 8%;\n        }\n        .close_100pay_btn {\n          background: #fff;\n          outline: none;\n          border: none;\n          padding: 6px 12px;\n          border-radius: 8px;\n          font-weight: 700;\n          color: #e94444;\n          cursor: pointer;\n        }\n        .body-overflow-hidden {\n        overflow: hidden;\n        }\n    "); let t = document.createElement("div"), n = document.createElement("div"), o = document.createElement("iframe"), a = document.createElement("p"), d = document.createElement("button"); const s = document.createTextNode("Close X"); t.insertAdjacentHTML("afterbegin", '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="margin: auto; background: none; display: block; shape-rendering: auto;" width="271px" height="271px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">\n    <path fill="none" stroke="#ffffff" stroke-width="8" stroke-dasharray="220.66647827148438 35.92244995117187" d="M24.3 30C11.4 30 5 43.3 5 50s6.4 20 19.3 20c19.3 0 32.1-40 51.4-40 C88.6 30 95 43.3 95 50s-6.4 20-19.3 20C56.4 70 43.6 30 24.3 30z" stroke-linecap="round" style="transform:scale(0.4);transform-origin:50px 50px">\n      <animate attributeName="stroke-dashoffset" repeatCount="indefinite" dur="1.4492753623188404s" keyTimes="0;1" values="0;256.58892822265625"></animate>\n    </path>\n    </svg>'), d.appendChild(s), a.style = "text-align: center; color: white; padding: 4px;", t.id = "100pay_loader", a.id = "close_100pay_btn", d.className = "btn text-white close_100pay_btn", d.id = "close_100pay_modal", a.appendChild(d), o.className = "show100Pay_modal", o.id = "show100PayModal", n.className = "show100Pay", n.id = "show100PayWrapper", n.appendChild(o), document.body.classList.add("body-overflow-hidden"), document.getElementById("show100Pay").style = "display: block;", document.getElementById("show100Pay").appendChild(a), document.getElementById("show100Pay").appendChild(t), document.getElementById("show100Pay").appendChild(n), document.getElementById("close_100pay_modal").addEventListener("click", (() => { this.closeModal(charge) })); let l = document.getElementById("show100PayModal"); l.addEventListener("load", (() => { document.getElementById("show100PayWrapper").classList.add("show") })), l.src = e.hosted_url } closeModal(e) { document.getElementById("show100PayWrapper").classList.remove("show"); let t = document.getElementById("show100PayWrapper"), n = document.getElementById("close_100pay_btn"); document.getElementById("100pay_loader").remove(), t.remove(), n.remove(), document.getElementById("show100Pay").style = "display: none;", document.body.classList.remove("body-overflow-hidden"), document.getElementById("100pay_style").remove(), e.onClose() } } const shop100Pay = new payWith100Pay; window.shop100Pay = shop100Pay, module.exports.shop100Pay = shop100Pay;
+let charge;
+class payWith100Pay {
+  constructor(config) { }
+  appendStyle(content) {
+    let style = document.createElement("STYLE");
+    style.id = "100pay_style";
+    style.type = "text/css";
+    style.appendChild(document.createTextNode(content));
+    document.head.appendChild(style);
+  }
+  setup(charge_data) {
+    charge = charge_data;
+    let api_key = charge.api_key;
+    delete charge.callback;
+    delete charge.api_key;
+    let data = charge;
+    fetch("https://api.shop100.co/api/v1/pay/charge", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "api-key": api_key,
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        this.createElements(data);
+      })
+      .catch((error) => {
+        charge.onError(error);
+      });
+  }
+  createElements(data) {
+    this.appendStyle(`
+        #show100Pay {
+        top: 0;
+        left: 0;
+        position: fixed;
+        background-color: #000000c2;
+        width: 100vw;
+        height: 100vh;
+        z-index: 1050;
+        display: none;
+        }
+        .show100Pay {
+        transition: 0.3s;
+        height: 100vh;
+        width: 100%;
+        max-width: 400px;
+        left: 50%;
+        transform: translateX(-50%);
+        position: fixed;
+        z-index: 1050;
+        top: 100%;
+        padding-top: 0;
+        border-radius: 16px 16px 0 0;
+        overflow: hidden;
+      }
+      .show100Pay .show100Pay_modal {
+        height: calc(100% - 8%);
+        width: 100%;
+        border: none;
+        border-radius: 16px 16px 0 0;
+        padding-bottom: 0;
+        }
+        .show100Pay.show {
+        top: 8%;
+        }
+        .close_100pay_btn {
+          background: #fff;
+          outline: none;
+          border: none;
+          padding: 6px 12px;
+          border-radius: 8px;
+          font-weight: 700;
+          color: #e94444;
+          cursor: pointer;
+        }
+        .body-overflow-hidden {
+        overflow: hidden;
+        }
+    `);
+    let loaderTemplate = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="margin: auto; background: none; display: block; shape-rendering: auto;" width="271px" height="271px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
+    <path fill="none" stroke="#ffffff" stroke-width="8" stroke-dasharray="220.66647827148438 35.92244995117187" d="M24.3 30C11.4 30 5 43.3 5 50s6.4 20 19.3 20c19.3 0 32.1-40 51.4-40 C88.6 30 95 43.3 95 50s-6.4 20-19.3 20C56.4 70 43.6 30 24.3 30z" stroke-linecap="round" style="transform:scale(0.4);transform-origin:50px 50px">
+      <animate attributeName="stroke-dashoffset" repeatCount="indefinite" dur="1.4492753623188404s" keyTimes="0;1" values="0;256.58892822265625"></animate>
+    </path>
+    </svg>`
+    let loaderWrapper = document.createElement("div");
+    let wrapper = document.createElement("div");
+    let iframe = document.createElement("iframe");
+    let closeButtonWrapper = document.createElement("p");
+    let closeButton = document.createElement("button");
+    const textnode = document.createTextNode("Close X");
+    loaderWrapper.insertAdjacentHTML("afterbegin", loaderTemplate);
+    closeButton.appendChild(textnode);
+    closeButtonWrapper.style =
+      "text-align: center; color: white; padding: 4px;";
+    loaderWrapper.id = "100pay_loader";
+    closeButtonWrapper.id = "close_100pay_btn";
+    closeButton.className = "btn text-white close_100pay_btn";
+    closeButton.id = "close_100pay_modal";
+    closeButtonWrapper.appendChild(closeButton);
+    iframe.className = "show100Pay_modal";
+    iframe.id = "show100PayModal";
+    wrapper.className = "show100Pay";
+    wrapper.id = "show100PayWrapper";
+    wrapper.appendChild(iframe);
+    document.body.classList.add("body-overflow-hidden");
+    document.getElementById("show100Pay").style = "display: block;";
+    document.getElementById("show100Pay").appendChild(closeButtonWrapper);
+    document.getElementById("show100Pay").appendChild(loaderWrapper);
+    document.getElementById("show100Pay").appendChild(wrapper);
+    let newCloseBtn = document.getElementById("close_100pay_modal");
+    newCloseBtn.addEventListener("click", () => {
+      this.closeModal(charge);
+    });
+    let newIframe = document.getElementById("show100PayModal");
+    newIframe.addEventListener("load", () => {
+      document.getElementById("show100PayWrapper").classList.add("show");
+    });
+    newIframe.src = data.hosted_url;
+  }
+  closeModal(charge) {
+    document.getElementById("show100PayWrapper").classList.remove("show");
+    let wrapper = document.getElementById("show100PayWrapper");
+    let close_btn = document.getElementById("close_100pay_btn");
+    let loaderWrapper = document.getElementById("100pay_loader");
+    loaderWrapper.remove();
+    wrapper.remove();
+    close_btn.remove();
+    document.getElementById("show100Pay").style = "display: none;";
+    document.body.classList.remove("body-overflow-hidden");
+    let style = document.getElementById("100pay_style");
+    style.remove();
+    charge.onClose();
+  }
+}
+const shop100Pay = new payWith100Pay();
+window.shop100Pay = shop100Pay;
+module.exports.shop100Pay = shop100Pay;
