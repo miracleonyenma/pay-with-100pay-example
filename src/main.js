9c64fcd21faa3d8c0c3c52e2fe00af11085b375f
Miracle Onyenma <42788603+miracleonyenma@users.noreply.github.com>
2023-03-30 00:29:42 +0000

    update: use browserfy to import checkout package to html

---
+++ src/main.js
@@ -0,0 +1,49 @@
+
+import { shop100Pay } from "@100pay-hq/checkout";
+
+const paymentForm = document.getElementById("paymentForm");
+paymentForm.addEventListener("submit", payWith100pay, false);
+function payWith100pay(e) {
+    e.preventDefault();
+    const email = document.getElementById("email-address").value;
+    const phone = document.getElementById("phone").value;
+    const amount = document.getElementById("amount").value;
+    const firstName = document.getElementById("first-name").value;
+    const lastName = document.getElementById("last-name").value;
+
+    shop100Pay.setup({
+        ref_id: "" + Math.floor(Math.random() * 1000000000 + 1),
+        api_key:
+            "LIVE;PK;eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBJZCI6IjYyOGIyMzdlMTlhYzUyMDAyYzU4ODU0ZCIsInVzZXJJZCI6IjYyOGIyMjhiMTlhYzUyMDAyYzU4ODUyNSIsImlhdCI6MTY1MzI4NTc1OH0.xbsD89ZxDffd6oTslQncQkIs0zAfGaiz8tOkCjLdgFs", // paste api key here
+        customer: {
+            user_id: "1", // optional
+            name: firstName + " " + lastName,
+            phone,
+            email,
+        },
+        billing: {
+            amount,
+            currency: "USD", // or any other currency supported by 100pay
+            description: "Test Payment",
+            country: "USA",
+            vat: 10, //optional
+            pricing_type: "fixed_price", // or partial
+        },
+        metadata: {
+            is_approved: "yes",
+            order_id: "OR2", // optional
+            charge_ref: "REF", // optionalm, you can add more fields
+        },
+        call_back_url: "http://localhost:8000/verifyorder/",
+        onClose: (msg) => {
+            alert("User closed payment modal.");
+        },
+        callback: (reference) => {
+            alert(`Transaction successful with id: ${reference}`);
+        },
+        onError: (error) => {
+            console.log(error);
+            alert("Sorry something went wrong pls try again.");
+        },
+    });
+}
---
+++ src/shop100Pay.js
@@ -0,0 +1,3 @@
+const {shop100Pay} = require("@100pay-hq/checkout")
+
+module.exports = shop100Pay
